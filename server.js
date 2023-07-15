42cc6cc38da44684c51508088c7690dc4108549c
José Mariotto <josemarioto@gmail.com>
2023-07-15 14:56:46 -0300

    Complete

--- server.js
+++ server.js
@@ -2,40 +2,45 @@
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/2317/2317963.png",
-        title: "Curso de programação",
-        category: "Estudo",
-        description: "Leandro Guarino e Luiz Eduardo Guarino são professores na área de TI há mais de 18 anos, e atuam com desenvolvimento de software no mercado desde o início dos anos 2000.",
-        url: "https://www.youtube.com/@2guarinos"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/1719/1719726.png",
-        title: "Exercicios",
-        category: "Saúde",
-        description: "Se você quer emagrecer de um jeito super prático você veio ao lugar certo! A Exercício em Casa é a primeira academia online do Brasil!",
-        url: "https://www.youtube.com/@exercicioemcasa"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/10812/10812647.png",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Muita natureza e músicas e sons para relaxar, estudar e dormir bem. Aproveitem! Muita paz a todos bem vindos ao meu canal! ",
-        url: "https://www.youtube.com/@CassioToledo"
-    },
-    {
-        img: "https://cdn-icons-png.flaticon.com/128/808/808439.png",
-        title: "Jogo de Videogame",
-        category: "Diversão",
-        description: "Steam é um software de gestão de direitos digitais criado pela Valve Corporation ou Valve L.L.C., de plataformas digitais como jogos e aplicativos de programação e fornece serviços facilitados como atualização automática de jogos",
-        url: "https://store.steampowered.com/?l=portuguese"
-    }
-]
+const db = require('./db')
+
+// const ideas = [
+//     {
+//         img: "https://cdn-icons-png.flaticon.com/128/2317/2317963.png",
+//         title: "Curso de programação",
+//         category: "Estudo",
+//         description: "Leandro Guarino e Luiz Eduardo Guarino são professores na área de TI há mais de 18 anos, e atuam com desenvolvimento de software no mercado desde o início dos anos 2000.",
+//         url: "https://www.youtube.com/@2guarinos"
+//     },
+//     {
+//         img: "https://cdn-icons-png.flaticon.com/128/1719/1719726.png",
+//         title: "Exercicios",
+//         category: "Saúde",
+//         description: "Se você quer emagrecer de um jeito super prático você veio ao lugar certo! A Exercício em Casa é a primeira academia online do Brasil!",
+//         url: "https://www.youtube.com/@exercicioemcasa"
+//     },
+//     {
+//         img: "https://cdn-icons-png.flaticon.com/128/10812/10812647.png",
+//         title: "Meditação",
+//         category: "Mentalidade",
+//         description: "Muita natureza e músicas e sons para relaxar, estudar e dormir bem. Aproveitem! Muita paz a todos bem vindos ao meu canal! ",
+//         url: "https://www.youtube.com/@CassioToledo"
+//     },
+//     {
+//         img: "https://cdn-icons-png.flaticon.com/128/808/808439.png",
+//         title: "Jogo de Videogame",
+//         category: "Diversão",
+//         description: "Steam é um software de gestão de direitos digitais criado pela Valve Corporation ou Valve L.L.C., de plataformas digitais como jogos e aplicativos de programação e fornece serviços facilitados como atualização automática de jogos",
+//         url: "https://store.steampowered.com/?l=portuguese"
+//     }
+// ]
 
 
 // Configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+// Habilitar uso do req.body
+server.use(express.urlencoded({extended: true}))
 
 // Configuração do nunjacks
 const nunjucks = require("nunjucks")
@@ -48,7 +53,14 @@ nunjucks.configure("views", {
 // e capturo o pedido do cliente para responder 
 server.get("/", function(req, res) {
 
-    const reversedIdeas = [...ideas].reverse()  
+   
+     db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+        
+        const reversedIdeas = [...rows].reverse()  
 
     let lastIdeas = []
     for (let idea of reversedIdeas) {
@@ -59,15 +71,56 @@ server.get("/", function(req, res) {
 
 
     return res.render("index.html", { ideas: lastIdeas })
+
+     })
+
+    
 })
 
 server.get("/ideias", function(req, res) {
-    
-    const reversedIdeas = [...ideas].reverse() 
 
-    return res.render("ideias.html", { ideas: reversedIdeas})
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        } 
+        const reversedIdeas = [...rows].reverse() 
+    
+        return res.render("ideias.html", { ideas: reversedIdeas})
+
+    })
+    
 })
 
+server.post("/", function (req, res) {
+//Inserir dados na tabela
+  const query = `
+  INSERT INTO  ideas(
+    image,
+    title,
+    category,
+    description,
+    link
+  ) VALUES (?,?,?,?,?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ]
+
+db.run(query, values, function(err) {
+    if (err) {
+        console.log(err)
+        return res.send("Erro no banco de dados!")
+    }
+
+   return res.redirect("/ideias")
+}) 
+})
 
 //Liguei meu servidor na porta 3000
 server.listen(3000)
